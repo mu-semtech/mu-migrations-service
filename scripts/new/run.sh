692b193ead0b6e5923d46df17e08add075cca5c2
Aad Versteden <madnificent@gmail.com>
2020-05-25 17:59:42 +0200

    Defensive script
    Let's assume at one point that a user did not actually read the
    documentation.  It would be nice if said user would be informed in a
    nicer manner.  That's what we're doing now.

--- scripts/new/run.sh
+++ scripts/new/run.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-MIGRATION_NAME=$1
+MIGRATION_FORMAT=$1
+MIGRATION_NAME=$2
+
+if [ -z $MIGRATION_FORMAT ]
+then
+    echo "Migration format not supplied, exiting"
+    exit 1
+fi
+
+if [ -z $MIGRATION_NAME ]
+then
+    echo "Migration format or migration name not supplied, exiting"
+    exit 1
+fi
+
+if [[ "$MIGRATION_FORMAT" != "sparql" && "$MIGRATION_FORMAT" != "ttl" ]]
+then
+    echo "Migration format should be either sparql or ttl."
+    echo "Received format '$MIGRATION_FORMAT'"
+    exit 1
+fi
+
 MIGRATION_TIMESTAMP=`date +%Y%0m%0d%0H%0M%0S`
-FILENAME="$MIGRATION_TIMESTAMP-$MIGRATION_NAME.sparql"
+FILENAME="$MIGRATION_TIMESTAMP-$MIGRATION_NAME.$MIGRATION_FORMAT"
 echo "Creating migration with name $FILENAME"
 mkdir -p /data/app/config/migrations/
 cd /data/app/config/migrations/
